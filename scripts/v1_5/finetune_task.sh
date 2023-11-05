9f4e1ebd0499dd8791b5713580648a0622ad2012
Dudu Moshe <dudu@lightricks.com>
2023-11-05 15:58:32 +0200

    params

--- scripts/v1_5/finetune_task.sh
+++ scripts/v1_5/finetune_task.sh
@@ -4,8 +4,8 @@ deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path liuhaotian/llava-v1.5-13b \
     --version v1 \
-    --data_path ./playground/data/llava_v1_5_mix665k.json \
-    --image_folder ./playground/data \
+    --data_path ./playground/data/dudu_finetune/dudu-finetune-data.json \
+    --image_folder ./playground/data/dudu_finetune/yoav-225 \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
@@ -14,10 +14,10 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-task \
+    --output_dir ./checkpoints/llava-v1.5-13b-dudu-task \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
-    --per_device_eval_batch_size 4 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
