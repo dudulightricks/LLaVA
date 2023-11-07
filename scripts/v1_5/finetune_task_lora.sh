3476320d41ec2552c4026d85d8572104ff7e985d
Dudu Moshe <dudu@lightricks.com>
2023-11-07 03:32:29 +0200

    bla1

--- scripts/v1_5/finetune_task_lora.sh
+++ scripts/v1_5/finetune_task_lora.sh
@@ -16,7 +16,7 @@ deepspeed llava/train/train_mem.py \
     --group_by_modality_length True \
     --bf16 True \
     --output_dir ./checkpoints/llava-v1.5-13b-task-lora-dudu \
-    --num_train_epochs 1000 \
+    --num_train_epochs 9 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
