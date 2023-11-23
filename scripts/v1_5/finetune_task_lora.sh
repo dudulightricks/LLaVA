7a5bbd4965104f84fea69126bca47ffff3a85ad3
Dudu Moshe <dudu@lightricks.com>
2023-11-23 15:34:45 +0200

    params of 1249.

--- scripts/v1_5/finetune_task_lora.sh
+++ scripts/v1_5/finetune_task_lora.sh
@@ -5,8 +5,8 @@ deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path liuhaotian/llava-v1.5-13b \
     --version v1 \
-    --data_path ./playground/data/dudu_finetune/dudu-finetune-data.json \
-    --image_folder ./playground/data/dudu_finetune/yoav-225 \
+    --data_path ./playground/data/dudu_finetune/dudu-finetune-data-1249.json \
+    --image_folder ./playground/data/dudu_finetune/yoav-1249 \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
@@ -15,7 +15,7 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-task-lora-dudu-small \
+    --output_dir ./checkpoints/llava-v1.5-13b-task-lora-dudu-1249 \
     --num_train_epochs 10 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 1 \
