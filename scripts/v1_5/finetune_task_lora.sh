7f8eb116f82255e09e9c0d2b67c4fab35da0eeef
Dudu Moshe <dudu@lightricks.com>
2023-12-14 14:31:18 +0200

    only 220k twice.

--- scripts/v1_5/finetune_task_lora.sh
+++ scripts/v1_5/finetune_task_lora.sh
@@ -3,7 +3,7 @@
 deepspeed llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path /opt/llava-with-lora-gpt2 \
+    --model_name_or_path liuhaotian/llava-v1.5-13b \
     --version v1 \
     --data_path ./playground/data/gpt-220-real.json \
     --image_folder ./playground/data/gpt-220-real \
@@ -15,14 +15,14 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-task-lora-gpt-real-1 \
+    --output_dir ./checkpoints/llava-v1.5-13b-task-lora-gpt-real-only-2 \
     --num_train_epochs 2 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
     --evaluation_strategy "epoch" \
     --save_strategy "steps" \
-    --save_steps 10 \
+    --save_steps 50 \
     --save_total_limit 1 \
     --learning_rate 2e-4 \
     --weight_decay 0. \
