1065470b4850cbf526324554924454098040a86d
Dudu Moshe <dudu@lightricks.com>
2024-02-19 15:24:11 +0200

    train omer 140k with eval on our best images model, 10 epochs

--- scripts/v1_5/finetune_task_lora.sh
+++ scripts/v1_5/finetune_task_lora.sh
@@ -3,10 +3,10 @@
 deepspeed llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path /opt/llava-with-lora-gpt2-real-only \
+    --model_name_or_path /opt/llava-images-1249-on-gpt2 \
     --version v1 \
-    --data_path ./playground/data/dudu_finetune/dudu-finetune-data-1249.json \
-    --image_folder ./playground/data/dudu_finetune/yoav-1249 \
+    --data_path /opt/omer-140k-gpt-images-train.json \
+    --image_folder /opt/omer-140k-gpt-images \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
@@ -15,15 +15,14 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-task-lora-1249-on-gpt2-real-only \
-    --num_train_epochs 3 \
+    --output_dir ./checkpoints/llava-images-lora-omer-140k-on-1249+gpt2 \
+    --num_train_epochs 10 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
     --evaluation_strategy "epoch" \
-    --save_strategy "steps" \
-    --save_steps 50 \
-    --save_total_limit 1 \
+    --save_strategy "epoch" \
+    --save_total_limit 3 \
     --learning_rate 2e-4 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
